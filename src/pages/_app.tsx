c1cb97da99066d8d88bf4395bbadd4d9450b7b88
DJPajares <dj.pajares@gmail.com>
2023-03-30 23:55:26 +0800

    feat: add head for app and index pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,27 +4,33 @@ import { ColorModeContext, useMode } from '@/config/themes';
 import '@/styles/globals.css';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { ProSidebarProvider } from 'react-pro-sidebar';
 
 function App({ Component, pageProps }: AppProps) {
   const { theme, colorMode } = useMode();
 
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
+    <>
+      <Head>
+        <title>Template App</title>
+      </Head>
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
 
-        <ProSidebarProvider>
-          <div className="app">
-            <SideNav />
-            <main className="content">
-              <TopBar />
-              <Component {...pageProps} />
-            </main>
-          </div>
-        </ProSidebarProvider>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+          <ProSidebarProvider>
+            <div className="app">
+              <SideNav />
+              <main className="content">
+                <TopBar />
+                <Component {...pageProps} />
+              </main>
+            </div>
+          </ProSidebarProvider>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+    </>
   );
 }
 
