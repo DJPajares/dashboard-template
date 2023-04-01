c88257480b6a3fc38a63affb9015cf53e5805360
DJPajares <dj.pajares@gmail.com>
2023-04-01 17:53:55 +0800

    feat: use mui appbar for header

--- src/components/global/Topbar.tsx
+++ src/components/global/Topbar.tsx
@@ -1,62 +1,77 @@
 import { ColorModeContext } from '@/config/themes';
-import { Box, Button, IconButton, InputBase, useTheme } from '@mui/material';
+import {
+  AppBar,
+  Box,
+  IconButton,
+  InputBase,
+  Toolbar,
+  useTheme
+} from '@mui/material';
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
 import SearchIcon from '@mui/icons-material/Search';
 import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 import { useContext } from 'react';
+import { useProSidebar } from 'react-pro-sidebar';
 
 const TopBar = () => {
   const theme = useTheme();
   const colors = theme.palette;
   const colorMode = useContext(ColorModeContext);
+  const { broken, toggleSidebar } = useProSidebar();
 
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        padding: 2,
-        backgroundColor: colors.background.default
-        // position: 'fixed',
-        // top: 0
-      }}
-    >
-      <Box
+    <AppBar position="sticky" elevation={0}>
+      <Toolbar
         sx={{
           display: 'flex',
-          alignItems: 'center',
-          backgroundColor: colors.background.paper,
-          borderRadius: 0.5
+          justifyContent: 'space-between',
+          backgroundColor: colors.background.default,
+          px: 1
         }}
+        disableGutters
       >
-        <InputBase placeholder="Search..." sx={{ px: 1 }} />
-        <IconButton>
-          <SearchIcon />
-        </IconButton>
-      </Box>
-      <Box sx={{ display: 'flex', alignItems: 'center' }}>
-        <IconButton onClick={colorMode.toggleColorMode}>
-          {theme.palette.mode === 'dark' ? (
-            <LightModeOutlinedIcon />
-          ) : (
-            <DarkModeOutlinedIcon />
-          )}
-        </IconButton>
-        <IconButton>
-          <NotificationsOutlinedIcon />
-        </IconButton>
-        <IconButton>
-          <SettingsOutlinedIcon />
-        </IconButton>
-        <IconButton>
-          <PersonOutlinedIcon />
-        </IconButton>
-      </Box>
-    </Box>
+        {broken && (
+          <IconButton onClick={() => toggleSidebar()}>
+            <MenuOutlinedIcon />
+          </IconButton>
+        )}
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            backgroundColor: colors.background.paper,
+            borderRadius: 0.5
+          }}
+        >
+          <InputBase placeholder="Search..." sx={{ px: 1 }} />
+          <IconButton>
+            <SearchIcon />
+          </IconButton>
+        </Box>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <IconButton onClick={colorMode.toggleColorMode}>
+            {theme.palette.mode === 'dark' ? (
+              <LightModeOutlinedIcon />
+            ) : (
+              <DarkModeOutlinedIcon />
+            )}
+          </IconButton>
+          <IconButton>
+            <NotificationsOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <SettingsOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <PersonOutlinedIcon />
+          </IconButton>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
 
